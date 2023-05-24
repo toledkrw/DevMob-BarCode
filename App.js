84edf10280827fae9b0a6c0e12763beab95c05c7
Matheus Toledo <57298489+toledkrw@users.noreply.github.com>
2023-05-24 23:03:50 +0000

    💄 removed damned css

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import Scanner from './src/components/BarCodeScanner';
+import Scanner from './src/components/Scanner';
 
 export default function App() {
   return (
@@ -14,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
