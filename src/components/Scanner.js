26c8927112329c2308460077ad9c4da3bd0f9a5c
Matheus <matheus.tererj@hotmail.com>
2023-05-25 14:01:18 -0300

    ✨ Scanner and File Saver

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,24 +1,39 @@
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as FileSystem from 'expo-file-system';
 
 export default Scanner = () => {
-
     const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(true);
-
+    const [scanned, setScanned] = useState(false);
+    const [scannedData, setScannedData] = useState(null);
 
     useEffect(() => {
         (async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status === "granted");
-        }
-        )();
+        })();
     }, []);
 
     const handleScan = ({ type, data }) => {
         setScanned(true);
-        console.log(`Codigo de barras do tipo ${type} e valor ${data} foi escaneado`);
+        setScannedData({ type, data });
+    };
+
+    const storeScannedInfo = async (data) => {
+        const fileUri = FileSystem.documentDirectory + "scanned_data.txt";
+        const fileInfo = await FileSystem.getInfoAsync(fileUri);
+
+        let content = "";
+        if (fileInfo.exists) {
+            const existingContent = await FileSystem.readAsStringAsync(fileUri);
+            content = existingContent + "\n";
+        }
+
+        content += data;
+
+        await FileSystem.writeAsStringAsync(fileUri, content);
+        console.log("Arquivo salvo em:", fileUri);
     };
 
     if (hasPermission === null) {
@@ -29,16 +44,17 @@ export default Scanner = () => {
         return <Text>Have no Camera access.</Text>;
     }
 
-
     return (
-        <View style={{flex:1}}>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleScan}
-                style={StyleSheet.absoluteFillObject}
-            />
-            <View style={{ margin: 20, position: 'absolute', bottom: 10 }}>
-                <Button title={'Press to scan'} onPress={() => setScanned(false)} />
+        <View style={{ flex: 1, backgroundColor: "#121212" }}>
+            <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleScan} style={StyleSheet.absoluteFillObject}/>
+            <View style={{ flex: 1, bottom: 20 }}>
+                {scanned && (
+                    <View style={{flex: 1,flexDirection: "row", justifyContent: "space-evenly", alignItems: "flex-end"}}>
+                        <Button title={"Press to scan again"} onPress={() => setScanned(false)}/>
+                        <Button title={"Press to store scanned info"} onPress={() => scannedData ? storeScannedInfo(scannedData).then(setScanned(false)) : undefined}/>
+                    </View>
+                )}
             </View>
         </View>
-    )
-}
+    );
+};
